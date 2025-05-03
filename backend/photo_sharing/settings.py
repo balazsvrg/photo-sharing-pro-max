e4aff985b215c9c0932e685236ffc17edb3f7acd
Balazs Varga <balazs.vrg98@gmail.com>
2025-05-03 23:15:15 +0200

    add Dockerfile

--- backend/photo_sharing/settings.py
+++ backend/photo_sharing/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'photo_sharing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'photosharing'),
+        'USER': os.environ.get('POSTGRES_USER', 'photosharing'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
